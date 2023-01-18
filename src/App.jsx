7b4fbc7ff7091e02cfc164dca380281799a2ada1
Kyle Holzinger <kyle@relaypayments.com>
2023-01-18 11:20:12 -0500

    [KH] unstyled app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,85 @@
-import "./App.css";
-import logo from "./logo.svg";
+import { useState } from 'react'
+import './App.css'
+import logo from './logo.svg'
 
 function App() {
+  const [name, setName] = useState('')
+  const [date, setDate] = useState('')
+  const [amount, setAmount] = useState('')
+
+  const [expenses, setExpenses] = useState([])
+
   return (
-    <div className="App" role="main">
-      <article className="App-article">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App">
+      <h1>Expense Tracker</h1>
+      <h2>Add new item:</h2>
+      <div class="fields">
+        <div class="row">
+          <label>
+            <span>Name:</span>
+            <input
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value)
+              }}
+            />
+          </label>
+        </div>
+        <div class="row">
+          <label>
+            <span>Date:</span>
+            <input
+              value={date}
+              placeholder="MM/DD/YYYY"
+              onChange={(e) => {
+                setDate(e.target.value)
+              }}
+            />
+          </label>
+          <label>
+            <span>Amount:</span>
+            <input
+              value={amount}
+              type="number"
+              onChange={(e) => {
+                setAmount(e.target.value)
+              }}
+            />
+          </label>
+        </div>
+        <button
+          onClick={() => {
+            setExpenses((x) => [...x, { name, date, amount }])
+          }}
         >
-          Learn React
-        </a>
-      </article>
+          Add Expense
+        </button>
+        <div>
+          <div class="row">
+            <div class="cell">Name</div>
+            <div class="cell">Date</div>
+            <div class="cell">Amount</div>
+          </div>
+          {expenses.map((x, i) => {
+            return (
+              <div class="row">
+                <div class="cell">{x.name}</div>
+                <div class="cell">{x.date}</div>
+                <div class="cell">{x.amount}</div>
+                <button
+                  onClick={() => {
+                    setExpenses(expenses.filter((_x, j) => i != j))
+                  }}
+                >
+                  x
+                </button>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
